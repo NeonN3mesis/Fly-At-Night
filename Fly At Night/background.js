7c7ab1ba26c936dd4920990c1a8b03c15b4c5aed
smarshall561 <129052650+smarshall561@users.noreply.github.com>
2023-04-10 06:25:10 -0400

    Update background.js

--- Fly At Night/background.js
+++ Fly At Night/background.js
@@ -1,10 +1,21 @@
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, { action: "toggleDarkMode" }, function(response) {
-    if (chrome.runtime.lastError) {
-      // The content script was not injected yet, try again
-      chrome.tabs.executeScript(tab.id, { file: "content.js" }, function() {
-        chrome.tabs.sendMessage(tab.id, { action: "toggleDarkMode" });
+chrome.browserAction.onClicked.addListener(function() {
+  if (chrome.extension.getStorage().get("nightMode") === "true") {
+    chrome.tabs.query({active: true}).then(function(tabs) {
+      tabs[0].executeScript({
+        code: "document.body.classList.add('night-mode');"
       });
-    }
-  });
+    });
+  } else {
+    chrome.tabs.query({active: true}).then(function(tabs) {
+      tabs[0].executeScript({
+        code: "document.body.classList.remove('night-mode');"
+      });
+    });
+  }
 });
+
+// In the file `contentscript.js`, add the following code:
+
+if (chrome.extension.getStorage().get("nightMode") === "true") {
+  document.body.classList.add("night-mode");
+}
