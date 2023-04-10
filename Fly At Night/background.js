583d438257cc4d1241c03b5eebbe83b2cbb358cc
smarshall561 <129052650+smarshall561@users.noreply.github.com>
2023-04-10 06:30:43 -0400

    Update background.js

--- Fly At Night/background.js
+++ Fly At Night/background.js
@@ -1,11 +1,17 @@
 chrome.browserAction.onClicked.addListener(function() {
   if (chrome.extension.getStorage().get("nightMode") === "true") {
+    if (!chrome.extension.isActive) {
+      chrome.browserAction.setActive(true);
+    }
     chrome.tabs.query({active: true}).then(function(tabs) {
       tabs[0].executeScript({
         code: "document.body.classList.add('night-mode');"
       });
     });
   } else {
+    if (chrome.extension.isActive) {
+      chrome.browserAction.setActive(false);
+    }
     chrome.tabs.query({active: true}).then(function(tabs) {
       tabs[0].executeScript({
         code: "document.body.classList.remove('night-mode');"
